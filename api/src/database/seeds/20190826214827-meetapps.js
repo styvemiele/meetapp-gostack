53ba518108d6e911d7b8e00d73858f2f0494e35d
styvemiele <styvemiele@gmail.com>
2019-10-27 14:38:57 -0300

    first commit

--- api/src/database/seeds/20190826214827-meetapps.js
+++ api/src/database/seeds/20190826214827-meetapps.js
@@ -3,17 +3,17 @@ const date = new Date();
 const subscribers = [...Array(51).keys()].filter(n => n !== 0);
 
 const description =
-  'O Community Challenge é uma competição global criada pelo Developer Circles from Facebook. Seu desafio é criar um software que utilize pelo menos uma das três tecnologias: React, Spark AR ou Games em HTML5.\n';
+  'Community Challenge é uma competição global criada pelo Developer Circles from Facebook. Seu desafio é criar um software que utilize pelo menos uma das três tecnologias: React, Spark AR ou Games em HTML5.\n';
 
-const location = 'Av. Paulista, 1234';
+const location = 'Av. Paulista, 159';
 
 const meetapps = [
   {
     id: 1,
-    title: 'React 360 - Community Challenge',
+    title: 'React - Community Challenge',
     description,
     location,
-    date: '2019-09-28T18:00:00.000Z',
+    date: '2019-11-28T18:00:00.000Z',
     owner_id: 1,
     banner_id: 1,
     updated_at: date,
@@ -23,10 +23,10 @@ const meetapps = [
   },
   {
     id: 2,
-    title: 'Vue.js summit 2019',
+    title: 'JS summit 2019',
     description,
     location,
-    date: '2019-09-30T18:00:00.000Z',
+    date: '2019-11-30T18:00:00.000Z',
     owner_id: 43,
     banner_id: 2,
     updated_at: date,
@@ -36,10 +36,10 @@ const meetapps = [
   },
   {
     id: 3,
-    title: 'ReactSP30 - Especial Frontend Week',
+    title: 'React - Frontend Week',
     description,
     location,
-    date: '2019-09-31T18:00:00.000Z',
+    date: '2019-12-31T18:00:00.000Z',
     owner_id: 27,
     banner_id: 3,
     updated_at: date,
@@ -52,7 +52,7 @@ const meetapps = [
     title: 'Rocketseat summit 2019',
     description,
     location,
-    date: '2019-09-01T18:00:00.000Z',
+    date: '2019-11-01T18:00:00.000Z',
     owner_id: 5,
     banner_id: 4,
     updated_at: date,
@@ -62,10 +62,10 @@ const meetapps = [
   },
   {
     id: 5,
-    title: 'Frontend SP - Especial FrontendWeek!',
+    title: 'Frontend - Especial FrontendWeek!',
     description,
     location,
-    date: '2019-09-11T18:00:00.000Z',
+    date: '2019-11-11T18:00:00.000Z',
     owner_id: 7,
     banner_id: 5,
     updated_at: date,
